61e73248f59a6a35ed7810f293a37191273d6438
1DanielWilliams <84046935+1DanielWilliams@users.noreply.github.com>
2022-06-24 16:17:12 -0700

    created the TVShow adapter to allow for the tvShow selection recycler view to be populated with data

---
+++ app/src/main/java/com/example/capstone/adapters/TVshowsAdapter.java
@@ -0,0 +1,57 @@
+package com.example.capstone.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.capstone.R;
+import com.example.capstone.models.Conversation;
+import com.example.capstone.models.Movie;
+
+import java.util.List;
+
+public class TVshowsAdapter extends RecyclerView.Adapter<TVshowsAdapter.ViewHolder> {
+    private Context context;
+    private List<Movie> tvShows;
+
+    public TVshowsAdapter(Context context, List<Movie> posts) {
+        this.context = context;
+        this.tvShows = tvShows;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_tv_movie, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Movie tvShow = tvShows.get(position);
+        holder.bind(tvShow);
+    }
+
+    @Override
+    public int getItemCount() {
+        return tvShows.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        //declare different views of post up here
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            // assign views to ids
+
+        }
+        public void bind(Movie tvShow) {
+            //bind data from event object to different views above
+
+        }
+    }
+}
