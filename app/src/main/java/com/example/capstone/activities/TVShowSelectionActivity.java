ad0dbc80ffe579ed33a48e526f745eca76debcd1
1DanielWilliams <84046935+1DanielWilliams@users.noreply.github.com>
2022-06-24 13:47:17 -0700

    Created layout for the view holder that will populate the recycler view of messages

---
+++ app/src/main/java/com/example/capstone/activities/TVShowSelectionActivity.java
@@ -0,0 +1,16 @@
+package com.example.capstone.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.capstone.R;
+
+public class TVShowSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.example.capstone.R.layout.activity_tvshow_selection);
+    }
+}
