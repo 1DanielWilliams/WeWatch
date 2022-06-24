252ab5bb94531c9bae0226f327c62dc43e295d96
1DanielWilliams <84046935+1DanielWilliams@users.noreply.github.com>
2022-06-24 16:30:40 -0700

    Created Conversation detail activity and filled out the UI

--- app/src/main/java/com/example/capstone/activities/ConversationDetailActivity.java
+++ app/src/main/java/com/example/capstone/activities/ConversationDetailActivity.java
@@ -1,34 +1,23 @@
 package com.example.capstone.activities;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.app.NavUtils;
 
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
 import android.widget.ImageButton;
 
 import com.example.capstone.R;
 
-import java.util.Objects;
-
-public class ProfileActivity extends AppCompatActivity {
-
-    private Toolbar toolbar;
+public class ConversationDetailActivity extends AppCompatActivity {
     private ImageButton upArrowProfile;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(com.example.capstone.R.layout.activity_profile);
+        setContentView(com.example.capstone.R.layout.activity_detail_conversation);
 
         upArrowProfile = findViewById(R.id.upArrowProfile);
-        toolbar = findViewById(R.id.toolbar);
-        toolbar.setContentInsetsAbsolute(0, 0);
 
         upArrowProfile.setOnClickListener(v -> finish());
-
     }
 }
